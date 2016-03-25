c69c6712f709fad2498595fd0b7d5c096ee1776a
Marvis_Zhao <zhaozhifei668@163.com>
2016-03-25 19:07:17 +0800

    提交新创建的工程

---
+++ CustomCollecttionViewLayout/CustomCollecttionViewLayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomCollecttionViewLayout
+//
+//  Created by imac on 16/3/25.
+//  Copyright © 2016年 suishouji. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
