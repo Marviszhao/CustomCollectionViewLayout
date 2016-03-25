bb36dd98692f2383d9a6641a5750e3b44655b989
Marvis_Zhao <zhaozhifei668@163.com>
2016-03-25 19:25:14 +0800

    添加正常工程文件

---
+++ CustomCollectionViewLayout-me/CustomCollectionViewLayout-me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCollectionViewLayout-me
+//
+//  Created by imac on 16/3/25.
+//  Copyright © 2016年 suishouji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomCollectionViewLayout-me/CustomCollectionViewLayout-me/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCollectionViewLayout-me
+//
+//  Created by imac on 16/3/25.
+//  Copyright © 2016年 suishouji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
