8fbca0d7563fef230bc7a582d265eee84423a0fd
Marvis_Zhao <zhaozhifei668@163.com>
2016-03-25 18:55:15 +0800

    添加基本工程

---
+++ CustomCollecttionViewLayout/CustomCollecttionViewLayout/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CustomCollecttionViewLayout
+//
+//  Created by imac on 16/3/25.
+//  Copyright © 2016年 suishouji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
